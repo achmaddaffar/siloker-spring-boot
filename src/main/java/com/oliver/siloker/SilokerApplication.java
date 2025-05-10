2d906f79ff6c49788a0e5087fb75e6c6f4c838fe
damieroliverroot <daffarres@gmail.com>
2025-05-10 16:37:20 +0700

    first commit

---
+++ src/main/java/com/oliver/siloker/SilokerApplication.java
@@ -0,0 +1,13 @@
+package com.oliver.siloker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliver/siloker/SilokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliver.siloker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
